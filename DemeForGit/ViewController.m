8a9ea3d332579d2ba2c0b37eae26b2016d399629
qaz1990 <13240310514@163.com>
2017-07-18 14:53:04 +0800

    commit init

---
+++ DemeForGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DemeForGit
+//
+//  Created by 秒贷金融 on 2017/7/3.
+//  Copyright © 2017年 秒贷金融. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ DemeForGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemeForGit
+//
+//  Created by 秒贷金融 on 2017/7/3.
+//  Copyright © 2017年 秒贷金融. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
